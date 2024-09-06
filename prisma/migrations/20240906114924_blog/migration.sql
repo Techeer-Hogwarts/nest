9b72d0cd219a7e99f21660d7742c7db64809942a
dlwhsk0 <hana.dev.314@gmail.com>
2024-09-06 21:02:00 +0900

    set: Blog 모델 세팅 issue: SCRUM-113

---
+++ prisma/migrations/20240906114924_blog/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "Blog" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+    "title" VARCHAR(100) NOT NULL,
+    "url" VARCHAR(100) NOT NULL,
+    "likeCount" INTEGER NOT NULL,
+    "viewCount" INTEGER NOT NULL,
+
+    CONSTRAINT "Blog_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Blog" ADD CONSTRAINT "Blog_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './modules/prisma/prisma.module';
+import { BlogsModule } from './modules/blogs/blogs.module';
 
 @Module({
-    imports: [PrismaModule],
+    imports: [PrismaModule, BlogsModule],
     controllers: [],
     providers: [],
 })
