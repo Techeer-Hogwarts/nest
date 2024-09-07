695669977761982b78e6cd8d6fe23a162e46c151
jihye9871 <jihye9871@naver.com>
2024-09-07 22:26:02 +0900

    set: app.module 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './modules/prisma/prisma.module';
 
+import { BookmarkModule } from './modules/bookmarks/bookmark.module';
+import { TeamModule } from './modules/teams/team.module';
+
 @Module({
-    imports: [PrismaModule],
+    imports: [PrismaModule, BookmarkModule, TeamModule],
     controllers: [],
     providers: [],
 })
