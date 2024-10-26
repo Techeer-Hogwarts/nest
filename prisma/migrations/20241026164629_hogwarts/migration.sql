49cbb5e1d94783602dc41c66fabb3909a4c4f229
suha <154861396+suhach0523@users.noreply.github.com>
2024-10-27 01:50:27 +0900

    refactor: 프리즈마 수정

--- prisma/migrations/20241026164629_hogwarts/migration.sql
+++ prisma/migrations/20241026164629_hogwarts/migration.sql
@@ -170,66 +170,18 @@ CREATE TABLE "Like" (
 );
 
 -- CreateTable
-CREATE TABLE "Calendar" (
-    "id" SERIAL NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
-
-    CONSTRAINT "Calendar_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "Event" (
     "id" SERIAL NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "isDeleted" BOOLEAN NOT NULL DEFAULT false,
-    "calendarId" INTEGER NOT NULL,
-    "isOnline" BOOLEAN NOT NULL DEFAULT false,
+    "category" VARCHAR(50) NOT NULL,
     "title" VARCHAR(200) NOT NULL,
-    "place" VARCHAR(200) NOT NULL,
-    "date" TIMESTAMP(3) NOT NULL,
-    "description" VARCHAR(200) NOT NULL,
     "startDate" TIMESTAMP(3) NOT NULL,
     "endDate" TIMESTAMP(3) NOT NULL,
+    "url" VARCHAR(200) NOT NULL,
 
     CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "JobInfo" (
-    "id" SERIAL NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
-    "calendarId" INTEGER NOT NULL,
-    "company" VARCHAR(100) NOT NULL,
-    "description" VARCHAR(200) NOT NULL,
-    "url" VARCHAR(200) NOT NULL,
-    "position" VARCHAR(200) NOT NULL,
-    "startDate" TIMESTAMP(3) NOT NULL,
-    "endDate" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "JobInfo_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "Conference" (
-    "id" SERIAL NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
-    "calendarId" INTEGER NOT NULL,
-    "title" VARCHAR(100) NOT NULL,
-    "url" VARCHAR(200) NOT NULL,
-    "startDate" TIMESTAMP(3) NOT NULL,
-    "endDate" TIMESTAMP(3) NOT NULL,
-    "deadline" TIMESTAMP(3) NOT NULL,
-    "price" INTEGER NOT NULL,
-    "isOnline" BOOLEAN NOT NULL DEFAULT false,
-
-    CONSTRAINT "Conference_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -291,15 +243,6 @@ ALTER TABLE "Bookmark" ADD CONSTRAINT "Bookmark_userId_fkey" FOREIGN KEY ("userI
 
 -- AddForeignKey
 ALTER TABLE "Like" ADD CONSTRAINT "Like_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Event" ADD CONSTRAINT "Event_calendarId_fkey" FOREIGN KEY ("calendarId") REFERENCES "Calendar"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "JobInfo" ADD CONSTRAINT "JobInfo_calendarId_fkey" FOREIGN KEY ("calendarId") REFERENCES "Calendar"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Conference" ADD CONSTRAINT "Conference_calendarId_fkey" FOREIGN KEY ("calendarId") REFERENCES "Calendar"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
