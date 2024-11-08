66b86258acf8f9e0e85666724d7aa84d66867684
suha <154861396+suhach0523@users.noreply.github.com>
2024-11-08 23:48:31 +0900

    feat: 커스텀 예외 처리 설정

---
+++ src/global/exception/custom.exception.ts
@@ -0,0 +1,13 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+export class NotFoundSessionException extends HttpException {
+    constructor() {
+        super('세션 게시물을 찾을 수 없습니다.', HttpStatus.NOT_FOUND);
+    }
+}
+
+export class NotFoundEventException extends HttpException {
+    constructor() {
+        super('이벤트를 찾을 수 없습니다.', HttpStatus.NOT_FOUND);
+    }
+}
