2639ffbb56a7532739ec192702d9f97f4a41d0f6
jihye9871 <jihye9871@naver.com>
2024-12-14 02:11:32 +0900

    fix: 프로젝트 멤버 관련 수정

---
+++ src/modules/projectMembers/entities/projectMember.entity.ts
@@ -0,0 +1,14 @@
+import { User } from '@prisma/client';
+import { ProjectMember } from '@prisma/client';
+
+export class ProjectMemberEntity implements ProjectMember {
+    id: number;
+    createdAt: Date;
+    updatedAt: Date;
+    isDeleted: boolean;
+    isLeader: boolean;
+    teamRole: string;
+    projectTeamId: number;
+    userId: number;
+    user: User;
+}
