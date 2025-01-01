31b26e5525d1f457c651e4a2e4d78200a1655581
jihye9871 <jihye9871@naver.com>
2025-01-02 00:06:33 +0900

    feat: ProjectTeam dto 구현

--- src/modules/projectTeams/dto/request/update.projectTeam.request.ts
+++ src/modules/projectTeams/dto/request/update.projectTeam.request.ts
@@ -1,125 +1,109 @@
 import {
     IsString,
-    IsNotEmpty,
     IsOptional,
     IsBoolean,
     IsInt,
-    Min,
     IsArray,
-    ArrayNotEmpty,
 } from 'class-validator';
-import { ApiHideProperty, ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiHideProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
 
-export class CreateProjectTeamRequest {
-    @IsNotEmpty()
+export class UpdateProjectTeamRequest {
+    @IsOptional()
     @IsString()
     @ApiProperty({
-        example: 'TecheerZip',
+        example: 'AI Development Project',
         description: '프로젝트 팀 이름',
     })
     name: string;
 
-    @IsNotEmpty()
+    @IsOptional()
     @IsString()
     @ApiProperty({
-        example: '테커를 위한 서비스',
+        example: 'https://github.com/ai-project',
+        description: '깃허브 링크',
+    })
+    githubLink: string;
+
+    @IsOptional()
+    @IsString()
+    @ApiProperty({
+        example: 'https://notion.com/ai-project',
+        description: '노션 링크',
+    })
+    notionLink: string;
+
+    @IsOptional()
+    @IsString()
+    @ApiProperty({
+        example: 'AI 기반의 새로운 기술을 개발하는 프로젝트입니다.',
         description: '프로젝트 설명',
     })
     projectExplain: string;
 
-    @IsNotEmpty()
+    @IsOptional()
     @IsInt()
-    @Min(0)
     @ApiProperty({
         example: 3,
-        description: '프론트엔드 모집 인원',
+        description: '프론트엔드 인원',
     })
     frontendNum: number;
 
-    @IsNotEmpty()
+    @IsOptional()
     @IsInt()
-    @Min(0)
     @ApiProperty({
         example: 2,
-        description: '백엔드 모집 인원',
+        description: '백엔드 인원',
     })
     backendNum: number;
 
-    @IsNotEmpty()
+    @IsOptional()
     @IsInt()
-    @Min(0)
     @ApiProperty({
         example: 1,
-        description: '데브옵스 모집 인원',
+        description: '데브옵스 인원',
     })
     devopsNum: number;
 
-    @IsNotEmpty()
+    @IsOptional()
     @IsInt()
-    @Min(0)
     @ApiProperty({
         example: 1,
-        description: 'UI/UX 모집 인원',
+        description: 'UI/UX 디자이너 인원',
     })
     uiuxNum: number;
 
-    @IsNotEmpty()
+    @IsOptional()
     @IsInt()
-    @Min(0)
     @ApiProperty({
         example: 1,
-        description: '데이터 엔지니어 모집 인원',
+        description: '데이터 엔지니어 인원',
     })
     dataEngineerNum: number;
 
     @IsOptional()
     @IsString()
     @ApiProperty({
-        example: '백엔드에 관심 있는 분을 모집합니다.',
+        example: '열정적으로 참여할 사람을 모집합니다.',
         description: '모집 설명',
     })
-    recruitExplain?: string;
+    recruitExplain: string;
 
-    @IsNotEmpty()
-    @IsBoolean()
-    @ApiProperty({
-        example: true,
-        description: '모집 여부',
-    })
-    isRecruited: boolean;
-
-    @IsNotEmpty()
+    @IsOptional()
     @IsBoolean()
     @ApiProperty({
         example: false,
-        description: '진행 여부',
+        description: '프로젝트 종료 여부',
     })
     isFinished: boolean;
 
     @IsOptional()
-    @IsString()
+    @IsBoolean()
     @ApiProperty({
-        example: 'https://github.com/techeerism',
-        description: '깃허브 링크',
+        example: true,
+        description: '모집 진행 여부',
     })
-    githubLink?: string;
-
-    @IsOptional()
-    @IsString()
-    @ApiProperty({
-        example: 'https://notion.so/techeerism',
-        description: '노션 링크',
-    })
-    notionLink?: string;
-
-    @IsArray()
-    @IsOptional()
-    @ApiProperty({
-        example: [1, 2, 3],
-        description: '스택 ID 배열',
-    })
-    stacks?: number[];
+    isRecruited: boolean;
 
     @IsOptional()
     @IsArray()
@@ -147,8 +131,8 @@ export class CreateProjectTeamRequest {
                 },
                 teamRole: {
                     type: 'string',
-                    description: '팀 내 역할',
-                    example: 'Frontend',
+                    description: '팀 역할',
+                    example: 'Backend',
                 },
             },
         },
@@ -164,6 +148,7 @@ export class CreateProjectTeamRequest {
                 teamRole: 'Backend',
             },
         ],
+        description: '프로젝트 멤버 목록',
     })
     projectMember: {
         userId: number;
@@ -173,7 +158,22 @@ export class CreateProjectTeamRequest {
 
     @IsOptional()
     @IsArray()
-    @ArrayNotEmpty()
+    @ApiProperty({
+        example: [1, 2, 3],
+        description: '삭제할 이미지 ID 배열',
+    })
+    deleteImages: number[];
+
+    @IsOptional()
+    @IsArray()
+    @ApiProperty({
+        example: [1, 2, 3],
+        description: '삭제할 멤버 ID 배열',
+    })
+    deleteMembers: number[];
+
+    @IsOptional()
+    @IsArray()
     @Transform(({ value }) => {
         try {
             return typeof value === 'string' ? JSON.parse(value) : value;
@@ -186,6 +186,11 @@ export class CreateProjectTeamRequest {
         items: {
             type: 'object',
             properties: {
+                id: {
+                    type: 'number',
+                    description: '팀 스택 ID',
+                    example: 1,
+                },
                 stack: {
                     type: 'string',
                     description: '스택 이름',
@@ -200,16 +205,23 @@ export class CreateProjectTeamRequest {
         },
         example: [
             {
+                id: 1,
                 stack: 'React.js',
                 isMain: true,
             },
             {
+                id: 2,
                 stack: 'Node.js',
                 isMain: false,
             },
         ],
+        description: '수정할 팀 스택 목록',
     })
-    teamStacks?: { stack: string; isMain: boolean }[];
+    teamStacks: {
+        id: number;
+        stack: string;
+        isMain: boolean;
+    }[];
 
     @ApiHideProperty() // Swagger에 표시되지 않도록 설정
     resultImages?: string[]; // 사용자가 입력하지 않음, 서버에서 자동 추가
