f46d1b387f513a44232f4dc3af7b613480d8b270
suha <154861396+suhach0523@users.noreply.github.com>
2025-02-09 21:55:14 +0900

    feat: 이벤트 생성 DTO

---
+++ src/modules/events/dto/response/creare.event.response.ts
@@ -0,0 +1,21 @@
+import { EventEntity } from '../../entities/event.entity';
+
+export class CreateEventResponse {
+    readonly id: number;
+    readonly userId: number;
+    readonly category: string;
+    readonly title: string;
+    readonly startDate: Date;
+    readonly endDate: Date;
+    readonly url: string;
+
+    constructor(eventEntity: EventEntity) {
+        this.id = eventEntity.id;
+        this.userId = eventEntity.userId;
+        this.category = eventEntity.category;
+        this.title = eventEntity.title;
+        this.startDate = eventEntity.startDate;
+        this.endDate = eventEntity.endDate;
+        this.url = eventEntity.url;
+    }
+}
