a4745bad1b78739ecbdb544750613481f9d81682
dlwhsk0 <hana.dev.314@gmail.com>
2025-02-23 03:44:00 +0900

    feat: 인덱스 업데이트 - url 추가 issue: SCRUM-242

--- src/modules/events/dto/request/index.event.request.ts
+++ src/modules/events/dto/request/index.event.request.ts
@@ -4,10 +4,12 @@ export class IndexEventRequest {
     readonly category: string;
     readonly id: string;
     readonly title: string;
+    readonly url: string;
 
     constructor(event: EventEntity) {
         this.category = event.category;
         this.id = event.id.toString();
         this.title = event.title;
+        this.url = event.url;
     }
 }
