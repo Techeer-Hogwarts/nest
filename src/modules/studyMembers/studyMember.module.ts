d6c522c63a7ae7f3cd8afa38be1c210502a34eb2
dongwoo <woomacho@gmail.com>
2025-03-16 20:33:10 +0900

    [BACKEND-10] study module 수정
    *변경사항
    1. Team, member: repository에서 service로 변경

--- src/modules/studyMembers/studyMember.module.ts
+++ src/modules/studyMembers/studyMember.module.ts
@@ -1,14 +1,14 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { AuthModule } from '../../modules/auth/auth.module';
-import { StudyMemberRepository } from './repository/studyMember.repository';
+import { AuthModule } from '../auth/auth.module';
 import { UserRepository } from '../users/repository/user.repository';
 import { StudyTeamModule } from '../studyTeams/studyTeam.module';
+import { StudyMemberService } from './studyMember.service';
 
 @Module({
     imports: [AuthModule, forwardRef(() => StudyTeamModule)], // forwardRef 사용
     controllers: [],
-    providers: [StudyMemberRepository, UserRepository, PrismaService],
-    exports: [StudyMemberRepository, UserRepository, PrismaService],
+    providers: [UserRepository, PrismaService, StudyMemberService],
+    exports: [StudyMemberService, UserRepository, PrismaService],
 })
 export class StudyMemberModule {}
