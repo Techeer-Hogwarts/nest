61ed11b10a7fb88f52971af8ad0301c4c5f8a487
kimzini <kimjinhee.dev@gmail.com>
2025-03-24 21:00:52 +0900

    refactor: auth 스웨거 분리

---
+++ src/common/dto/auth/request/verfiy.emailCode.request.ts
@@ -0,0 +1,18 @@
+import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class VerifyEmailCodeRequest {
+    @IsString()
+    @ApiProperty({
+        example: 'user@example.com',
+        description: '사용자 이메일',
+    })
+    email: string;
+
+    @IsString()
+    @ApiProperty({
+        example: '111111',
+        description: '이메일 인증 코드',
+    })
+    code: string;
+}
