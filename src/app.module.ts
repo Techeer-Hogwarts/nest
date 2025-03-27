a408c31fad7bb82fa5842343ece2db62312b7c64
Ryan <leehahnwoong@gmail.com>
2025-03-26 20:21:20 -0700

    fix: test header

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { InfraModule } from './infra/infra.module';
 import { ApiModule } from './api/api.module';
 import { CoreModule } from './core/core.module';
 import { CommonModule } from './common/common.module';
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { TraceLoggingMiddleware } from './middle';
 
 @Module({
     imports: [TracingModule, InfraModule, ApiModule, CoreModule, CommonModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+    configure(consumer: MiddlewareConsumer): void {
+        consumer
+            .apply(TraceLoggingMiddleware) // Apply the middleware globally
+            .forRoutes('*'); // Apply to all routes (you can specify routes here if needed)
+    }
+}
