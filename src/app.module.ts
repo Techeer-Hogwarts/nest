4329b3436b087354a5d358ef40ab81460ad7e327
Ryan <leehahnwoong@gmail.com>
2025-03-26 17:28:28 -0700

    tracing module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { ApiModule } from './api/api.module';
 import { CoreModule } from './core/core.module';
 import { CommonModule } from './common/common.module';
 import { Module } from '@nestjs/common';
+import { TracingModule } from './infra/openTelemetry/tracing.module';
 
 @Module({
-    imports: [InfraModule, ApiModule, CoreModule, CommonModule],
+    imports: [InfraModule, ApiModule, CoreModule, CommonModule, TracingModule],
 })
 export class AppModule {}
---
+++ src/infra/openTelemetry/tracing.service.ts
@@ -0,0 +1,43 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
+import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
+import { PrismaInstrumentation } from '@prisma/instrumentation';
+// import { PgInstrumentation } from '@opentelemetry/instrumentation-pg';
+import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
+// import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+import { CustomWinstonLogger } from '../../common/logger/winston.logger';
+import { resourceFromAttributes } from '@opentelemetry/resources';
+
+@Injectable()
+export class TracingService implements OnModuleInit, OnModuleDestroy {
+    private sdk: NodeSDK;
+
+    private readonly logger = new CustomWinstonLogger();
+
+    async onModuleInit(): Promise<void> {
+        this.sdk = new NodeSDK({
+            resource: resourceFromAttributes({
+                [ATTR_SERVICE_NAME]: 'nestjs-app',
+            }),
+            traceExporter: new OTLPTraceExporter({
+                url: 'http://otel-collector:4318/v1/traces', // Adjust if needed
+            }),
+            instrumentations: [
+                new HttpInstrumentation(),
+                new ExpressInstrumentation(),
+                new PrismaInstrumentation(),
+                // new PgInstrumentation(),
+            ],
+        });
+
+        await this.sdk.start();
+        this.logger.log('✅ OpenTelemetry tracing started');
+    }
+
+    async onModuleDestroy(): Promise<void> {
+        await this.sdk.shutdown();
+        this.logger.log('OpenTelemetry tracing stopped');
+    }
+}
