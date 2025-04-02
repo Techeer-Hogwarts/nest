0d5e1a1a721f31f67260e77285967bb3cdfb0300
kimzini <kimjinhee.dev@gmail.com>
2025-04-03 06:54:25 +0900

    fix: html 코드 파일 분리

--- src/core/auth/auth.service.ts
+++ src/core/auth/auth.service.ts
@@ -25,6 +25,7 @@ import {
     AuthVerificationFailedException,
 } from './exception/auth.exception';
 import { ServerException } from '../../common/exception/base.exception';
+import { authEmailTemplate } from './templete/auth.templete';
 
 @Injectable()
 export class AuthService {
@@ -163,82 +164,14 @@ export class AuthService {
         }
 
         const subject = '이메일 인증 코드';
-        const htmlContent = `
-            <!DOCTYPE html>
-            <html lang="ko">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <style>
-                    body {
-                        font-family: 'Arial', sans-serif;
-                        background-color: #FE9142;
-                        padding: 20px;
-                    }
-                    .email-container {
-                        max-width: 600px;
-                        background-color: #ffffff;
-                        border-radius: 8px;
-                        margin: 0 auto;
-                        padding: 20px;
-                        box-shadow: 0 2px 10px rgba(0,0,0,0.1);
-                        text-align: center;
-                    }
-                    .email-header {
-                        font-size: 24px;
-                        font-weight: bold;
-                        color: #333333;
-                        margin-bottom: 20px;
-                    }
-                    .email-body {
-                        font-size: 16px;
-                        color: #555555;
-                        line-height: 1.6;
-                    }
-                    .verification-code-container {
-                        background-color: #FE9142;
-                        color: #ffffff;
-                        font-size: 24px;
-                        font-weight: bold;
-                        padding: 15px 0;
-                        border-radius: 8px;
-                        margin: 20px 0;
-                        width: 50%;
-                        margin-left: auto;
-                        margin-right: auto;
-                    }
-                    .footer {
-                        font-size: 12px;
-                        color: #888888;
-                        margin-top: 20px;
-                    }
-                </style>
-            </head>
-            <body>
-                <div class="email-container">
-                    <div class="email-header">테커집 인증 코드</div>
-                    <div class="email-body">
-                        아래의 인증 코드를 사용하여 이메일 인증을 완료하세요.<br>
-                        <div class="verification-code-container">
-                            ${verificationCode}
-                        </div>
-                        인증 코드는 <strong>5분간 유효</strong>합니다.
-                    </div>
-                    <div class="footer">
-                        본 메일은 테커 회원 인증을 위해 발송된 이메일입니다.<br>
-                        이메일 인증에 문제가 있다면, 관리자에게 문의주세요.
-                    </div>
-                </div>
-            </body>
-            </html>
-        `;
+
         // 이메일 전송
         try {
             await this.transporter.sendMail({
                 from: this.configService.get<string>('EMAIL_FROM_EMAIL'),
                 to: email,
                 subject: subject,
-                html: htmlContent,
+                html: authEmailTemplate(verificationCode),
             });
             this.logger.debug('이메일 전송 완료', AuthService.name);
         } catch (error) {
