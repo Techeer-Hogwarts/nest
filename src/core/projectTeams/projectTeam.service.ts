7a83d8c23ef8d5c12d69c5b018179be2ffaf0b65
Ryan <leehahnwoong@gmail.com>
2025-04-09 23:29:01 -0700

    fix: remove where in findmanyprojectteamleaders

--- src/core/projectTeams/projectTeam.service.ts
+++ src/core/projectTeams/projectTeam.service.ts
@@ -1402,7 +1402,6 @@ export class ProjectTeamService {
                 name: true,
                 projectMember: {
                     where: {
-                        id: projectTeamId,
                         isDeleted: false,
                         isLeader: true,
                         status: MemberStatus.APPROVED,
