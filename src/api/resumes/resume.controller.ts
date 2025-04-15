edacc012ed315a621b1fcd65ec3dc629d4964ae6
kjeongh <wellsy001012@gmail.com>
2025-04-15 23:25:48 +0900

    refactor: 스웨거 파일 분리

--- src/api/resumes/resume.controller.ts
+++ src/api/resumes/resume.controller.ts
@@ -23,6 +23,7 @@ import { JwtAuthGuard } from '../../core/auth/jwt.guard';
 import { Request } from 'express';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { CustomWinstonLogger } from '../../common/logger/winston.logger';
+import { CreateResumeDoc, DeleteResumeDoc, GetBestResumesDoc, GetResumeDoc, GetResumeListDoc, GetResumesByUserDoc, UpdateMainResumeDoc } from './resume.docs';
 
 @ApiTags('resumes')
 @Controller('resumes')
@@ -34,10 +35,7 @@ export class ResumeController {
 
     @UseGuards(JwtAuthGuard)
     @Get()
-    @ApiOperation({
-        summary: '이력서 목록 조회 및 검색',
-        description: '이력서를 조회하고 검색합니다.',
-    })
+    @GetResumeListDoc()
     async getResumeList(
         @Query() query: GetResumesQueryRequest,
     ): Promise<GetResumeResponse[]> {
@@ -55,11 +53,7 @@ export class ResumeController {
 
     @UseGuards(JwtAuthGuard)
     @Get('/best')
-    @ApiOperation({
-        summary: '인기 이력서 목록 조회',
-        description:
-            '2주간의 (조회수 + 좋아요수*10)을 기준으로 인기 이력서를 조회합니다.',
-    })
+    @GetBestResumesDoc()
     async getBestResumes(
         @Query() query: PaginationQueryDto,
     ): Promise<GetResumeResponse[]> {
@@ -79,44 +73,7 @@ export class ResumeController {
     @Post()
     @UseInterceptors(FileInterceptor('file')) // 파일 업로드 처리
     @ApiConsumes('multipart/form-data') // Swagger에서 파일 업로드 지원
-    @ApiOperation({
-        summary: '이력서 생성',
-        description:
-            '파일과 폼 데이터를 사용해 이력서를 생성합니다.\n\n카테고리는 RESUME, PORTFOLIO, ICT, OTHER 입니다.',
-    })
-    @ApiBody({
-        schema: {
-            type: 'object',
-            properties: {
-                file: {
-                    type: 'string',
-                    format: 'binary', // 파일 필드
-                    description: '업로드할 파일',
-                },
-                category: {
-                    type: 'string',
-                    example: 'PORTFOLIO',
-                    description: '이력서 타입',
-                },
-                position: {
-                    type: 'string',
-                    example: 'BACKEND',
-                    description: '이력서 포지션',
-                },
-                title: {
-                    type: 'string',
-                    example: '스타트업',
-                    description: '이력서 제목',
-                },
-                isMain: {
-                    type: 'boolean',
-                    example: true,
-                    description: '이력서 대표 여부',
-                },
-            },
-            required: ['file', 'category', 'position', 'title', 'isMain'], // 필수 필드
-        },
-    })
+    @CreateResumeDoc()
     async createResume(
         @Req() request: any,
         @UploadedFile() file: Express.Multer.File, // 파일 데이터
@@ -134,10 +91,7 @@ export class ResumeController {
 
     @UseGuards(JwtAuthGuard)
     @Get(':resumeId')
-    @ApiOperation({
-        summary: '단일 이력서 조회',
-        description: '지정된 ID의 이력서를 조회합니다.',
-    })
+    @GetResumeDoc()
     async getResume(
         @Param('resumeId', ParseIntPipe) resumeId: number,
     ): Promise<GetResumeResponse> {
@@ -155,10 +109,7 @@ export class ResumeController {
 
     @UseGuards(JwtAuthGuard)
     @Get('/user/:userId')
-    @ApiOperation({
-        summary: '유저 별 이력서 목록 조회',
-        description: '지정된 유저의 이력서를 조회합니다.',
-    })
+    @GetResumesByUserDoc()
     async getResumesByUser(
         @Param('userId') userId: number,
         @Query() query: PaginationQueryDto,
@@ -177,10 +128,7 @@ export class ResumeController {
 
     @UseGuards(JwtAuthGuard)
     @Delete(':resumeId')
-    @ApiOperation({
-        summary: '이력서 삭제',
-        description: '지정된 ID의 이력서를 삭제합니다.',
-    })
+    @DeleteResumeDoc()
     async deleteResume(
         @Req() request: Request,
         @Param('resumeId', ParseIntPipe) resumeId: number,
@@ -197,11 +145,7 @@ export class ResumeController {
 
     @UseGuards(JwtAuthGuard)
     @Patch(':resumeId')
-    @ApiOperation({
-        summary: '메인 이력서 지정',
-        description:
-            '사용자가 올린 이력서들 중 메인으로 표시할 이력서를 변경합니다.',
-    })
+    @UpdateMainResumeDoc()
     async updateMainResume(
         @Req() request: any,
         @Param('resumeId', ParseIntPipe) resumeId: number,
