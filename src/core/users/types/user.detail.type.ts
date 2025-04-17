a28e7a90bc1ee88107b8db30c12cfd4f09d1a76b
kimzini <kimjinhee.dev@gmail.com>
2025-04-17 19:05:32 +0900

    refactor: UserDetail 타입 분리

--- src/core/users/types/user.detail.type.ts
+++ src/core/users/types/user.detail.type.ts
@@ -5,29 +5,31 @@ import {
     UserExperience,
 } from '@prisma/client';
 
+export type ProjectTeamInfo = {
+    id: number;
+    name: string;
+    isDeleted: boolean;
+    resultImages: { imageUrl: string }[];
+    mainImages: { imageUrl: string }[];
+};
+
+export type ProjectMemberInfo = ProjectMember & {
+    projectTeam: ProjectTeamInfo | null;
+};
+
+export type StudyTeamInfo = {
+    id: number;
+    name: string;
+    isDeleted: boolean;
+    resultImages: { imageUrl: string }[];
+};
+
+export type StudyMemberInfo = StudyMember & {
+    studyTeam: StudyTeamInfo | null;
+};
+
 export type UserDetail = User & {
-    projectMembers?: (ProjectMember & {
-        projectTeam: {
-            id: number;
-            name: string;
-            isDeleted: boolean;
-            resultImages: {
-                imageUrl: string;
-            }[];
-            mainImages: {
-                imageUrl: string;
-            }[];
-        } | null;
-    })[];
-    studyMembers?: (StudyMember & {
-        studyTeam: {
-            id: number;
-            name: string;
-            isDeleted: boolean;
-            resultImages: {
-                imageUrl: string;
-            }[];
-        } | null;
-    })[];
+    projectMembers?: ProjectMemberInfo[];
+    studyMembers?: StudyMemberInfo[];
     experiences?: UserExperience[];
 };
