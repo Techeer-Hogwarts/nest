85b55f00943afbe4f76faa13bc3e5165a7df22f0
yoonjungeun1 <angal2310@tukorea.ac.kr>
2025-05-02 04:03:03 +0900

    fix: import 오류 해결

--- src/api/events/event.controller.ts
+++ src/api/events/event.controller.ts
@@ -12,13 +12,7 @@ import {
     UseGuards,
 } from '@nestjs/common';
 
-import { EventService } from '../../core/events/event.service';
-
-import { CreateEventRequest } from '../../common/dto/events/request/create.event.request';
-import { GetEventListQueryRequest } from '../../common/dto/events/request/get.event.query.request';
-
-import { CreateEventResponse } from '../../common/dto/events/response/create.event.response';
-import { GetEventResponse } from '../../common/dto/events/response/get.event.response';
+import { Request } from 'express';
 
 import {
     CreateEventDoc,
@@ -31,7 +25,7 @@ import {
 
 import { CreateEventRequest } from '../../common/dto/events/request/create.event.request';
 import { GetEventListQueryRequest } from '../../common/dto/events/request/get.event.query.request';
-import { CreateEventResponse } from '../../common/dto/events/response/create.event.response';
+import { CreateEventResponse } from '../../common/dto/events/response/create.event.response';
 import { GetEventResponse } from '../../common/dto/events/response/get.event.response';
 import { CustomWinstonLogger } from '../../common/logger/winston.logger';
 import { JwtAuthGuard } from '../../core/auth/jwt.guard';
